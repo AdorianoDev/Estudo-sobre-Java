e97afd42be9805908dedbf4e138be3f792a511bd
AdorianoDev <32521883+AdorianoDev@users.noreply.github.com>
2017-10-10 14:41:08 -0300

    Aulas 11 e 12.

---
+++ aula11/SequenciaEscape.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula11;
+
+public class SequenciaEscape {
+
+	public static void main(String[] args) {
+
+		//"Hello, World!"
+		System.out.print("\"Hello,World!\"\n\r");
+		// 1\4
+		System.out.println("1\\4");
+		//println = \n\r porém se vc criar mais um texto, ele não fica na linha de baixo, ele pula a linha.
+	}
+
+}
